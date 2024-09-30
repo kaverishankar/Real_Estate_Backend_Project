28034ed1418aceccc90b47c3d3c7b989ec0d21ea
Kaveri shankar M <71606190+kaverishankar@users.noreply.github.com>
2024-09-30 13:40:31 +0530

    Update mail-utils.js

--- db-utils/mail-utils.js
+++ db-utils/mail-utils.js
@@ -3,12 +3,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// Create transporter object using Gmail service
+
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: process.env.GMAIL_USER, // Your Gmail address
-    pass: process.env.GMAIL_PASSWORD, // Your Gmail app-specific password
+    user: process.env.GMAIL_USER, 
+    pass: process.env.GMAIL_PASSWORD, 
   },
 });
 
